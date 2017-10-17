fe198efc3422abd7fddb4f65f3458997981da48a
Simon Suewer <simon.suewer@inside-m2m.de>
2017-10-17 10:49:32 +0200

    Human

--- Human.java
+++ Human.java
@@ -17,23 +17,12 @@ public class Human extends Ship
         //move left
         if(Greenfoot.isKeyDown("a"))
         {
-<<<<<<< HEAD
-            
-                this.move(this.get_moveSpeed() * (-1));
-            
-=======
-                this.move(this.get_moveSpeed() * (-1));
->>>>>>> 64c5493d8ff3c9601db86b02e552f0920731e87c
+            this.move(this.get_moveSpeed() * (-1));
         }
         //move right
         if(Greenfoot.isKeyDown("d"))
         {
-<<<<<<< HEAD
-              this.move(this.get_moveSpeed());
-            
-=======
-                this.move(this.get_moveSpeed());
->>>>>>> 64c5493d8ff3c9601db86b02e552f0920731e87c
+            this.move(this.get_moveSpeed());
         }
     }    
 }
