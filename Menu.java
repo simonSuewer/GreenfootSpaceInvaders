918cfde9a0554c71c82688e1867f56636bd9275f
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-10-17 13:23:25 +0200

    edit

--- Menu.java
+++ Menu.java
@@ -2,25 +2,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Menu extends World
 {
-    MouseInfo mouseInfo;
     
+   MouseInfo mouseInfo;
     public Menu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 800, 1);
         initial();
-        int button, mouseY, mouseX = 0;
-         MouseInfo mouse = Greenfoot.getMouseInfo();
-        if(mouse!=null){
-            button = mouse.getButton();
-            if(button == 1 && Greenfoot.mouseClicked(null)) //Add this else it will be transferred to your mouse all the time, now just when you click.
-            {
-            mouseX=mouse.getX();
-            mouseY=mouse.getY();
-            }
-            System.out.println(mouseX);
-        }
-        
+
+  
     }     
     
     public void initial() {
@@ -29,4 +19,9 @@ public class Menu extends World
         addObject(new ButtonSettings(), 400, 600);
         addObject(new ButtonExit(), 400, 700);
     }
+    
+    public void act() {
+        
+    }
+    
 }
