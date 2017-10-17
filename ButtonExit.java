e2b13e0dd0a27be04a878ab317aa4ac1a36bec5b
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-10-17 13:38:32 +0200

    edit

--- ButtonExit.java
+++ ButtonExit.java
@@ -8,12 +8,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ButtonExit extends MenuActor
 {
-    /**
-     * Act - do whatever the ButtonExit wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+       private boolean onThis=false;
+ 
+ 
+    public void act() {
+        if(Greenfoot.mouseMoved(null))
+            onThis = Greenfoot.mouseMoved(this);
+        if(onThis)
+        {
+            System.out.println("Hover");
+            setImage("button_exit-hover.png"); 
+        }
+        else
+        {
+            System.out.println("notHover");
+            setImage("button_exit.png"); 
+        }
+    }  
 }
