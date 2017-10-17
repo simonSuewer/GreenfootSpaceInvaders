cac4797ff87b0f53ec61b981f18f8046b194d8ba
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-10-17 10:15:50 +0200

    Commit

---
+++ Menu.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends World
+{
+
+    /**
+     * Constructor for objects of class Menu.
+     * 
+     */
+    public Menu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
