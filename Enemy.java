173325803472ee984b26874cc448f11af7cbcd12
mbeckmann1 <32855103+mbeckmann1@users.noreply.github.com>
2017-10-17 12:11:52 +0200

    enemy movement

--- Enemy.java
+++ Enemy.java
@@ -15,6 +15,15 @@ public class Enemy extends Ship
     public void act() 
     {
         // Add your action code here.
-        move(5);
+        move(this.get_moveSpeed());
+        if(this.isAtEdge())
+        {
+            this.set_moveSpeed(this.get_moveSpeed()*(-1));
+        }
+        else if(this.getObjectsInRange(58, Enemy.class).toString()!="[]")
+        {
+            this.set_moveSpeed(this.get_moveSpeed()*(-1));
+            move(this.get_moveSpeed());
+        }
     }    
 }
