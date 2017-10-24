16897807a585214b95e2910d5aaa4be293a886ea
LucasReich <32844947+LucasReich@users.noreply.github.com>
2017-10-24 22:12:55 +0200

    Upgrade ohne Shop und nächstes Lvl

---
+++ ButtonNextLvl.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ButtonNextLvl here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ButtonNextLvl extends Upgrade
+{
+    /**
+     * Act - do whatever the ButtonNextLvl wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(this.isTouching(HumanShot.class))
+        {
+            
+        }
+    }    
+}
