310a3dbcf63030b4b0c568686da1a113d020507c
TimHaase <32848668+TimHaase@users.noreply.github.com>
2017-11-14 09:57:08 +0100

    Score

---
+++ Score.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+
+/**
+ * Write a description of class Score here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Score extends Actor
+{
+    int score = 0;
+    /**
+     * Act - do whatever the Score wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        setImage(new GreenfootImage("Score : " + score, 24, greenfoot.Color.GREEN,greenfoot.Color.BLACK));
+    }   
+    
+    
+    public void addScore()
+    {
+        score++;
+    }
+}
