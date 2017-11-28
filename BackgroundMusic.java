64fb51649da74c5e5bec079a476f344873bbd718
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-11-28 13:12:03 +0100

    edit add backgroundsounds

--- BackgroundMusic.java
+++ BackgroundMusic.java
@@ -3,11 +3,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class BackgroundMusic
 { 
       private static GreenfootSound soundBackgroundMusic;
-      public static void initialSound(int NumberOfSong) {
+
+      
+      private static void playBackgroundMusic(int NumberOfSong) {
           soundBackgroundMusic = new GreenfootSound("backgroundmusic/" + NumberOfSong + ".wav");
         }
-      
-      public void playBackgroundMusic(int NumberOfSong) {
-          soundBackgroundMusic.play();
-        }
+        
+        
+    public void stopped(int level)
+    {
+      playBackgroundMusic(level);
+      soundBackgroundMusic.setVolume(0);  //change bg to what you declared the file as
+      soundBackgroundMusic.pause();
+    }
+    public void started(int level)
+    {
+       playBackgroundMusic(level);
+       soundBackgroundMusic.setVolume(100);    //change bg to what you declared the file as
+       soundBackgroundMusic.playLoop();
+    }
 }
