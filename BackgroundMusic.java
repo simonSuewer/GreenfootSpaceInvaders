c224027dec2fdfb75e97c9d096807fe5274ae860
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-11-28 09:00:46 +0100

    edit

--- BackgroundMusic.java
+++ BackgroundMusic.java
@@ -1,6 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class BackgroundMusic
-{
+{ 
+      private static GreenfootSound soundBackgroundMusic;
+      public static void initialSound(int NumberOfSong) {
+          soundBackgroundMusic = new GreenfootSound("backgroundmusic/" + NumberOfSong + ".wav");
+        }
       
+      public void playBackgroundMusic(int NumberOfSong) {
+          soundBackgroundMusic.play();
+        }
 }
