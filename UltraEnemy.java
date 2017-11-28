3cb1bcd1bfed410f64ddfbe026a891475552ed38
SimonSuewer <simon.suewer@web.de>
2017-11-28 11:53:53 +0100

    lvl 6.9

--- UltraEnemy.java
+++ UltraEnemy.java
@@ -20,10 +20,15 @@ public class UltraEnemy extends Enemy
         super();
         Safegame load = new Safegame();
         this.health = load.getLvl() * load.getLvl();
+        if((Greenfoot.getRandomNumber(2)) == 1)
+        {
+           changeHorizontal=!changeHorizontal; 
+        }
     }
     
     public void act() 
     {
+        //System.out.println(!getWorld().getObjects(Enemy.class).isEmpty());
         //shot
         if(shotCount != 0) shotCount -= 1;
         if((Greenfoot.getRandomNumber(100) < 1) && (getWorld().getObjects(EnemyShot.class).size())< max_shot)
@@ -52,13 +57,20 @@ public class UltraEnemy extends Enemy
            if(newCounter == 0)
            {
                newCounter = 40;
-               getWorld().addObject(new UltraSven(), 110+(Greenfoot.getRandomNumber(740)),140);
+               getWorld().addObject(new UltraSven(), 110+(Greenfoot.getRandomNumber(580)),140);
            }
            else{
                newCounter -= 1;
            }
             
        
+       }
+       
+       
+       
+       if((getX() >= 750 || getX() <= 50) && !(getY() >= 750) )
+       {
+            changeHorizontal=!changeHorizontal;
        }
        
        if(getY() >= 750)
