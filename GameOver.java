94796b86aceff0644d8770bb7b8c46be8a5332dc
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-12-05 12:25:53 +0100

    edit

---
+++ GameOver.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOver extends World
+{
+
+    /**
+     * Constructor for objects of class GameOver.
+     * 
+     */
+    public GameOver()
+    {    
+       
+        super(800, 800, 1);
+        setBackground(new GreenfootImage("backgrounds/game_over.png"));
+    }
+}
