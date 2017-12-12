b69d4c5bbea27a3556ae3100000e205c40a8e575
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-12-12 08:36:50 +0100

    edit

---
+++ BackgroundPictures.java
@@ -0,0 +1,8 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class BackgroundPictures 
+{
+    public static void setBackground(World w) {
+        w.setBackground(new GreenfootImage("backgrounds/2.png"));
+    }
+}
