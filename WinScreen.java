47fb944131f5c940f27f80f0973dcc475c349417
Steven Streller <32670306+StevenStreller@users.noreply.github.com>
2017-12-12 12:11:38 +0100

    edit

--- WinScreen.java
+++ WinScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WinScreen extends World
 {
-
+    Safegame savegame = new Safegame();
     /**
      * Constructor for objects of class WinScreen.
      * 
@@ -18,6 +18,7 @@ public class WinScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 800, 1);
         initial();
+        savegame.reset();
     }
     public void initial() {
         addObject(new BackgroundHeader(), 400, 125);
